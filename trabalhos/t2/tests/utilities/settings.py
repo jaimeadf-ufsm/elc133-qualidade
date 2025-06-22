c5e5e8eb2f19594a37cde87dedcc0afd8d0dbf62
lgtozevich <lgtozevich@inf.ufsm.br>
2025-06-22 20:57:43 -0300

    Revert "feat: add tests for QIS search page"
    This reverts commit 48ae7085c57587e854c9b32905bf307e41c44909.

--- trabalhos/t2/tests/utilities/settings.py
+++ trabalhos/t2/tests/utilities/settings.py
@@ -3,5 +3,3 @@ import os
 class Settings:
     MAIN_BASE_URL = os.getenv('BASE_URL', 'https://www.uni-rostock.de')
     BSZ_BASE_URL = os.getenv('BSZ_BASE_URL', 'https://www.bsz.uni-rostock.de')
-    QIS_BASE_URL = os.getenv('QIS_BASE_URL', 'https://lsf.uni-rostock.de')
-    QIS_SEARCH_URL = f"{QIS_BASE_URL}/qisserver/rds?state=change&type=5&moduleParameter=veranstaltungSearch&nextdir=change&next=search.vm&subdir=veranstaltung&_form=display&function=search&clean=y&category=veranstaltung.search&navigationPosition=lectures%2Csearch&breadcrumb=searchLectures&topitem=lectures&subitem=search"
